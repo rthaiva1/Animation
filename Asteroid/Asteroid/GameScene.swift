3a8b4d1a004f90e624e83921a241e0451e233b07
Rosh Sugathan Thaivalappil <roshsugathanthaivalappil@Roshs-MacBook-Pro.local>
2019-10-12 23:04:05 -0400

    Bullets added

--- Asteroid/Asteroid/GameScene.swift
+++ Asteroid/Asteroid/GameScene.swift
@@ -12,6 +12,7 @@ class GameScene: SKScene {
     
     var attacktime:Timer!
     var player:SKSpriteNode!
+    var bulletnode:SKSpriteNode!
     var totalVillians = ["Villian","Villian2"]
     var field:SKEmitterNode!
     var count:intmax_t = 0
@@ -27,6 +28,27 @@ class GameScene: SKScene {
         attacktime = Timer.scheduledTimer(timeInterval: 0.75, target: self,selector: #selector(addVillian), userInfo: nil, repeats: true)
     }
     
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        bullet()
+    }
+    func bullet()
+    {
+        bulletnode = SKSpriteNode(imageNamed: "Player")
+        bulletnode.position = player.position
+        bulletnode.position.y = bulletnode.position.y + 2
+        
+        bulletnode.physicsBody = SKPhysicsBody(circleOfRadius: bulletnode.size.width/2)
+        bulletnode.physicsBody?.isDynamic = true
+        self.addChild(bulletnode)
+        
+        let anim:TimeInterval = 0.3
+        
+        var actionArray = [SKAction]()
+        
+        actionArray.append(SKAction.move(to: CGPoint(x: player.position.x,y: self.frame.size.height + 10),duration:anim))
+        actionArray.append(SKAction.removeFromParent())
+        bulletnode.run(SKAction.sequence(actionArray))
+    }
     @objc func addVillian()
     {
         if(count == 0)
